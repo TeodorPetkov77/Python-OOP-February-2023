e59722c18b9985c3b361972ca139847ed762f37b
RebelShark <tpetkov93@gmail.com>
2023-02-25 17:37:24 +0200

    x

--- Useful Functions/time_converter.py
+++ Useful Functions/time_converter.py
@@ -9,7 +9,6 @@ def convert_seconds_to_time(total_seconds):
     seconds = total_seconds % 60
     minutes = total_seconds // 60 % 60
     hours = total_seconds // 3600
-
     return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
 
 
