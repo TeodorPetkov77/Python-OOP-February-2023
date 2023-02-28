593e52a28f565f8e8f92987572bbeccb88082f64
RebelShark <tpetkov93@gmail.com>
2023-02-28 17:18:25 +0200

    x

--- Exercises/04_classes_and_objects_exercise/08_library/project/library.py
+++ Exercises/04_classes_and_objects_exercise/08_library/project/library.py
@@ -14,15 +14,14 @@ class Library:
 
     def get_book(self, author: str, book_name: str, days_to_return: int, user):
 
-        for book in self.books_available[author]:
-            if book == book_name:
-                user.books.append(book)
-                self.books_available[author].remove(book)
-                if user.username not in self.rented_books:
-                    self.rented_books[user.username] = {}
-                self.rented_books[user.username][book_name] = days_to_return
-                return f"{book_name} successfully rented for the " \
-                       f"next {days_to_return} days!"
+        if book_name in self.books_available[author]:
+            user.books.append(book_name)
+            self.books_available[author].remove(book_name)
+            if user.username not in self.rented_books:
+                self.rented_books[user.username] = {}
+            self.rented_books[user.username][book_name] = days_to_return
+            return f"{book_name} successfully rented for the " \
+                   f"next {days_to_return} days!"
 
         for username, books in self.rented_books.items():
             for book, days in books.items():
